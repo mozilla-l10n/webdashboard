7021dc4760aff07f5c98a86b1a1610fee84a09c9
TheoChevalier <theo.chevalier11@gmail.com>
2014-04-15 15:14:27 -0700

    Create new views for Australis (australis_core, australis_mozorg, australis_all)

--- data/project.php
+++ data/project.php
@@ -1,11 +1,8 @@
 <?php
 // This is the list of the tracked pages
-$pages = [
-    'default' => [
-        ['file' => 'main.lang',
-         'site' => 0],
-    ],
-    'australis' => [
+
+
+$australis_core = [
         ['file' => 'firefox/desktop/index.lang',
          'site' => 0],
         ['file' => 'firefox/desktop/fast.lang',
@@ -17,11 +14,26 @@ $pages = [
         ['file' => 'firefox/australis/firefox_tour.lang',
          'site' => 0],
         ['file' => 'firefox/sync.lang',
-         'site' => 0],
-        ['file' => 'apr2014.lang',
-         'site' => 6],
+         'site' => 0]
+    ];
 
+$australis_mozorg = $australis_core;
+$australis_mozorg[] = ['file' => 'firefox/new.lang','site' => 0];
+$australis_mozorg[] = ['file' => 'mozorg/home.lang', 'site' => 0];
+$australis_mozorg[] = ['file' => 'tabzilla/tabzilla.lang', 'site' => 0];
+$australis_mozorg[] = ['file' => 'main.lang', 'site' => 0];
+
+$australis_all = $australis_mozorg;
+$australis_all[] = ['file' => 'apr2014.lang', 'site' => 6];
+
+$pages = [
+    'default' => [
+        ['file' => 'main.lang',
+         'site' => 0],
     ],
+    'australis_core' => $australis_core,
+    'australis_mozorg' => $australis_mozorg,
+    'australis_all' => $australis_all,
     'firefox_os' => [
         ['file' => 'firefox/os/index.lang',
          'site' => 0],
