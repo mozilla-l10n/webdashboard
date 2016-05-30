49b40c444da6f5f6302e253dc6efc229c9bf3fa4
Francesco Lodolo <flod@lodolo.net>
2016-05-30 17:07:32 +0200

    Drop local list of locales, use directly Langchecker's (#144)

--- app/inc/init.php
+++ app/inc/init.php
@@ -1,6 +1,7 @@
 <?php
 namespace Webdashboard;
 
+use Cache\Cache;
 use Json\Json;
 
 // We always work with UTF8 encoding
@@ -22,16 +23,29 @@ if (file_exists($settings_filename)) {
     exit;
 }
 
-// Include data about all supported locales
-include __DIR__ . '/../data/locales.php';
-
-// Re-usable JSON object
-$json_object = new Json;
-
 // Cache class
 define('CACHE_ENABLED', true);
 define('CACHE_PATH', __DIR__ . '/../../cache/');   // This folder needs to be writable by PHP
 define('CACHE_TIME', 15 * 60);    // Default: 15 minutes
+
+// Get the list of all supported locales from Langchecker
+$json_locales = new Json;
+$cache_id = 'webdashboard_locales';
+if (! $locales = Cache::getKey($cache_id, 60 * 60)) {
+    $locales = $json_locales
+        ->setURI(LANG_CHECKER . '?action=listlocales&project=langchecker&json')
+        ->fetchContent();
+    Cache::setKey($cache_id, $locales);
+}
+unset($json_locales);
+if (empty($locales)) {
+    echo "<h1>Error: missing list of supported locales</h1>";
+    echo "<p>There was an error retrieving the list of supported locales from Langchecker. Please check the configuration file.</p>";
+    exit;
+}
+
+// Re-usable JSON object
+$json_object = new Json;
 
 // For debugging
 if (DEBUG) {
